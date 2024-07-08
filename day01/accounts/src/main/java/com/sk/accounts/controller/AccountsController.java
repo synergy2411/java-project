f79b674430a7e67b5e1975959b7184cd57230e05
Sumit Khandelwal <synergy2411@outlook.com>
2024-07-08 11:37:01 +0530

    tables added

---
+++ day01/accounts/src/main/java/com/sk/accounts/controller/AccountsController.java
@@ -0,0 +1,18 @@
+package com.sk.accounts.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AccountsController {
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World");
+    }
+}
