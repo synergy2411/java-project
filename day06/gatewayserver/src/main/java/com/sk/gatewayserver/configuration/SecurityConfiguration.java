2764a3729c96f7335e5dd40638e1e784fa932b7a
Sumit Khandelwal <synergy2411@outlook.com>
2024-07-12 10:45:24 +0530

    auth added

--- day06/gatewayserver/src/main/java/com/sk/gatewayserver/configuration/SecurityConfiguration.java
+++ day06/gatewayserver/src/main/java/com/sk/gatewayserver/configuration/SecurityConfiguration.java
@@ -1,5 +1,28 @@
 package com.sk.gatewayserver.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.authorizeExchange(exchange -> exchange
+                .pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("/synergybank/accounts/**").authenticated()
+                .pathMatchers("/synergybank/loans/**").authenticated()
+                .pathMatchers("/synergybank/cards/**").authenticated()
+                ).oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()));
+
+        httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return httpSecurity.build();
+    }
 }
