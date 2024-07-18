26593c2b390c5957ba9c0c2c93a4d1cf2839ce06
Sumit Khandelwal <synergy2411@outlook.com>
2024-07-18 16:38:27 +0530

    k8s manifest added

--- day09/security-demo/src/main/java/com/sk/security_demo/jwt/CustomJwt.java
+++ day09/security-demo/src/main/java/com/sk/security_demo/jwt/CustomJwt.java
@@ -5,11 +5,13 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Collection;
+import java.util.List;
 
 public class CustomJwt extends JwtAuthenticationToken {
 
     private String firstName;
     private String lastName;
+    private List<String> roles;
 
     public CustomJwt(Jwt jwt, Collection<? extends GrantedAuthority> authorities) {
         super(jwt, authorities);
@@ -29,5 +31,13 @@ public class CustomJwt extends JwtAuthenticationToken {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
